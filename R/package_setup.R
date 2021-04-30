28a4803c9cc1eacdadfeb3266f6ccb8ef63482c2
zackarno <zackarno@gmail.com>
2021-04-30 15:11:58 +0300

    init commit

---
+++ R/package_setup.R
@@ -0,0 +1,4 @@
+# usethis::create_package("<path_to_folder/name_of_package>")
+# usethis::use_tutorial("<name-of-learnr-file>", "<Title You'd Like the User to See>")
+# usethis::use_readme_rmd()
+# usethis::use_git()
